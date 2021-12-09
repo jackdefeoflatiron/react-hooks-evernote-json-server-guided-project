cfdc899f29c15865426d92c830fabcb22b63f8f9
Jack DeFeo <jackd3243@gmail.com>
2021-12-09 13:08:45 -0500

    start up

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 
 function NoteContainer() {
+  const [notes, setNotes] = useState([])
+  
+  const fetchNotes = () => {
+    fetch("http://localhost:3000/notes")
+    .then(res => res.json())
+    .then(notesData => {
+      console.log(notesData)
+      setNotes(notesData)
+    })
+  }
+  useEffect(fetchNotes, [])
+
+  
   return (
     <>
       <Search />
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NoteItem from "./NoteItem";
 
-function NoteList() {
+function NoteList({notes, NoteItem}) {
+  const renderNotes = notes.map(notes => <NoteItem key={notes.id} notes={notes} />)
   return (
     <ul>
-      {/* Render list of notes here... */}
+      {renderNotes}
       <NoteItem />
     </ul>
   );
